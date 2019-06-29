4de9ff565b88ae4475ce39218d2b28ac752ba32d
Nathan Wells <sungkhum@gmail.com>
2019-06-29 14:30:28 -0700

    updated helper function in mergeSort.py

--- mergeSort.py
+++ mergeSort.py
@@ -22,25 +22,15 @@ def mergeHelper(listA, listB):
     """This is a helper function for mergeSort that 
     sorts and combines two lists recursively.
     """
-    mergedList = []
-    if listA == [] and listB == []:
-        return mergedList
+    if listA == []:
+        return listB
+    elif listB == []:
+        return listA
+    elif listA[0] <= listB[0]:
+        return [listA[0]] + mergeHelper(listA[1:], listB)
     else:
-        if listA == [] and listB != []:
-            mergedList.extend(listB[0:])
-            return mergedList
-        elif listA != [] and listB == []:
-            mergedList.extend(listA[0:])
-            return mergedList
-        elif listA[0] <= listB[0]:
-            mergedList.append(listA[0])
-            mergedList.extend(mergeHelper(listA[1:], listB))
-            return mergedList
-        else:
-            mergedList.append(listB[0])
-            mergedList.extend(mergeHelper(listA, listB[1:]))
+        return [listB[0]] + mergeHelper(listA, listB[1:])
             
-    return mergedList
 
 
 print(mergeSort([1,11,4,5,6,7,3,10,5]))
